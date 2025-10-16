e2e1fa4cb14cf9cd54f444c408d26df7372b8a97
MargotM <margot@ls-a.fr>
2025-10-16 10:24:46 +0200

    workflow test

---
+++ ressources/s5/margot-ma/projets/ci-cd-symfony/src/Controller/BuggyController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BuggyController extends AbstractController
+{
+
+    #[Route('/buggy', name: 'app_buggy')]
+    public function index(): JsonResponse
+    {
+        return $this->json($this->getData());
+    }
+
+    /**
+     * @return array<string, number>
+     */
+    private function getData(): array
+    {
+        return [
+            'key1' => 1,
+            'key2' => 2,
+        ];
+    }
+}
